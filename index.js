ba6c391fb3e496af4c02cdfd75d9fbbb71a8b55d
Adam Kaplun <adamk@qwilt.com>
2024-01-27 11:01:29 +0200

    👌 IMPROVE:

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
+
+const clearConsole = require('clear-any-console');
+
+// Clears the console.
+clearConsole();
 console.log(`
 Baba
 
-Baba learning Node.js
+Baba learning Node.js 1
 `);
